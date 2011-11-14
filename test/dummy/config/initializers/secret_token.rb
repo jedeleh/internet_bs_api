70dcc1537b7b6a9552eff72f4007abc596b2bdc8
JD Kaplan <jaydel@gmail.com>
2011-11-14 07:53:50 -0600

    accidentally checked in junk I didn't want.

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '2812cd934aaa066b63f68bd9f67ebc72d89ac857a4f53f89e3b56cb054d3cf3b3587f06ecf2c99a85e6d1943af2cb4c553114b7f636d9650f5ecf9b5caf4d73d'
--- test/internet_bs_api_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class InternetBsApiTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, InternetBsApi
-  end
-end
