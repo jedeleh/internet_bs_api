7de85725aa880bb85224ba3e610f5effc334debf
JD Kaplan <jaydel@gmail.com>
2011-11-14 12:28:04 -0600

    refactored to simplify the module namespace. added a basic test to make sure that the api can be accessed via include

---
+++ lib/internet_bs_api/email_forward.rb
@@ -0,0 +1,15 @@
+module InternetBsApi
+  module Domain
+    def add_email_forward
+    end
+
+    def remove_email_forward
+    end
+
+    def update_email_forward
+    end
+
+    def list_email_forwards
+    end
+  end
+end
